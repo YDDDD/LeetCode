adbfb56afb8a7607b9246dcbf280dbd32faebe42
YDDDD <yellowdavid@vip.qq.com>
2017-10-24 12:08:38 +0800

    Update CombinationSum.v1.cs

--- Algorithm/39.Combination Sum/CombinationSum.v1.cs
+++ Algorithm/39.Combination Sum/CombinationSum.v1.cs
@@ -1,29 +1,27 @@
-//Runtime:565ms
-//beats 73.84%
-public class Solution
-{
-    public IList<IList<int>> CombinationSum(int[] candidates, int target)
+//Runtime:545ms
+//beats 86.63%
+    public class Solution
     {
-        List<IList<int>> ret = new List<IList<int>>();          
-        Array.Sort(candidates);
-        Push(candidates, target, 0, new List<int>(), ret);
-
-        return ret;
-    }
-
-    public void Push(int[] candidates, int target, int cursor, List<int> stack, List<IList<int>> ret)
-    {
-        int sum = stack.Sum();
-        for (int i = cursor; i < candidates.Length && candidates[i] <= target && sum + candidates[i] <= target; i++)
+        public IList<IList<int>> CombinationSum(int[] candidates, int target)
         {
-            stack.Add(candidates[i]);
-            if (sum + candidates[i] == target){
+            List<IList<int>> ret = new List<IList<int>>();          
+            Array.Sort(candidates);
+            Push(candidates, target, 0, new List<int>(), ret);
+
+            return ret;
+        }
+
+        public void Push(int[] candidates, int target, int cursor, List<int> stack, List<IList<int>> ret)
+        {
+            if(target < 0) return;
+            for (int i = cursor; i < candidates.Length && candidates[i] <= target; i++)
+            {
+                stack.Add(candidates[i]);
+                if (candidates[i] == target)
                     ret.Add(stack.ToArray());                    
+                else
+                    Push(candidates, target - candidates[i], i, stack, ret);
                 stack.RemoveAt(stack.Count - 1);
-                break;
             }
-            Push(candidates, target, i, stack, ret);
-            stack.RemoveAt(stack.Count - 1);
         }
     }
-}
