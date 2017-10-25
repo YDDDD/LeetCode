c03be61323d6e5e14f49749370b0ee8cd6b488e7
YDDDD <yellowdavid@vip.qq.com>
2017-10-25 11:45:21 +0800

    Create TrappingRainWater.V1.c

---
+++ Algorithm/42. Trapping Rain Water/TrappingRainWater.V1.c
@@ -0,0 +1,40 @@
+//Runtime 12ms
+//Beats 12%   T_T  DO BETTER AT V2
+
+void Search(int *height, int start, int from, int maxofOtherSide, int* ret) {
+	if ((from == -1 && start < 0) || (from != 0 && start == from)) return;
+
+	int maxIndex = start, maxVal = 0;
+	int step = from < 0 ? -1 : 1;
+	for (int i = start; i != from; i += step) {
+		if (height[i] >= maxVal) {
+			maxIndex = i;
+			maxVal = height[i];
+		}
+	}
+
+	int depth = maxofOtherSide > maxVal ? maxVal : maxofOtherSide;
+	for (int i = start; i != maxIndex; i += step) {
+		*ret += height[i] < depth ? depth - height[i] : 0;
+	}
+
+	Search(height, maxIndex + step, from, depth, ret);
+}
+
+int trap(int* height, int heightSize) {
+	if (heightSize < 3) return 0;
+	int ret = 0;
+	
+	int maxIndex = 0, maxVal = 0;
+	for (int i = 0; i < heightSize; i++) {
+		if (height[i] > maxVal) {
+			maxIndex = i;
+			maxVal = height[i];
+		}
+	}
+
+	Search(height, maxIndex + 1, heightSize, maxVal, &ret);
+	Search(height, maxIndex - 1, -1, maxVal, &ret);
+
+	return ret;
+}
