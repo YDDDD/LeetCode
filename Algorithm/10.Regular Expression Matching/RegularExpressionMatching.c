b9652984b8f047f1c3619100997fe3277dd82ae1
YDDDD <yellowdavid@vip.qq.com>
2017-11-13 11:23:26 +0800

    Create RegularExpressionMatching.c

---
+++ Algorithm/10.Regular Expression Matching/RegularExpressionMatching.c
@@ -0,0 +1,33 @@
+bool isMatch(char* s, char* p) {
+    if(*s == '\0') {
+        if(*p == '\0') {
+            return true;//exit here
+        } else if(*p == '*') {
+            return isMatch(s - 1, p + 1) || isMatch(s, p + 1);
+        } else if((++p) && *p == '*') {
+            return isMatch(s, ++p);
+        } else {
+            return false;
+        }
+    }
+    switch (*p) {
+        case '*':
+            if(isMatch(s - 1, p + 1)) {
+                return true;
+            }
+            while(*s != '\0' && (*s == *(p - 1) || *(p - 1) == '.')) {
+                if(isMatch(s, p + 1)) return true;
+                s++;
+            }
+            return isMatch(s, ++p);
+        case '.':
+            return isMatch(++s, ++p);
+            break;
+        case '\0':
+            return false;
+        default:
+            if(*p == *s) return isMatch(++s, ++p);
+            else if(*(++p) == '*') return isMatch(s, ++p);
+            else return false;
+    }
+}
