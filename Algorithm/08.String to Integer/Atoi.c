71c984042cf4bdb70b1c151eea25dd3abe79353b
YDDDD <yellowdavid@vip.qq.com>
2017-11-14 11:52:44 +0800

    Create Atoi.c

---
+++ Algorithm/08.String to Integer/Atoi.c
@@ -0,0 +1,15 @@
+int myAtoi(char* str) {
+    int value = 0, sign = 1;
+    while(*str == ' ') str++;
+    if(*str == '+' || *str == '-') {
+        sign = *str == '-' ? -1 : 1;
+        str++;
+    }
+    
+    while(*str >= '0' && *str <= '9') {
+        if(value > INT_MAX / 10 || (value == INT_MAX / 10 && *str - '0' > 7)) return sign == 1 ? INT_MAX : INT_MIN;
+        value = value * 10 + (*str - '0');
+        str++;
+    }
+    return value * sign;
+}
