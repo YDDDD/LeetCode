bfa44a0edc84877a2c7aee7017215a868b7f1817
YDDDD <yellowdavid@vip.qq.com>
2017-11-16 15:56:19 +0800

    Update WildcardMatching.c

--- Algorithm/43.Wildcard Matching/WildcardMatching.c
+++ Algorithm/43.Wildcard Matching/WildcardMatching.c
@@ -1,25 +1,25 @@
-//TLE..
+//Runtiem : 28ms
+//Beat 76.19%
+
+int myMatch(char *s, char*p) {
+	//0:false 1:true 2:match fail and end loop
+	if (*s == '\0') {
+		while (*p == '*') ++p;
+		return *p ? 0 : 1;
+	}
+	if (*p == '*') {
+		if (*(p - 1) == '*') return myMatch(s, ++p);
+		do {
+			int ret = myMatch(s++, p + 1);
+			if(ret == 0) continue;
+			if(ret == 2) return 2;
+			if(ret == 1) return 1;
+		} while (*s);
+		return myMatch(s, p) == 1 ? 1 : 2;
+	}
+	else return *p == '?' || *s == *p ? myMatch(++s, ++p) : 0;
+}
 
 bool isMatch(char* s, char* p) {
-    if(*s == '\0') {
-        switch(*p) {
-            case '\0': return true;
-            case '*': return isMatch(s, ++p);
-            default: return false;
-        }
-    }
-    
-    switch(*p) {
-        case '?' : return isMatch(++s, ++p);
-        case '*' :
-            if(*(p - 1) == '*') return isMatch(s, ++p);
-            while(*s != '\0') {
-                if(isMatch(s++, p + 1)) return true;
-            }
-            return isMatch(s, ++p);
-        case '\0': return false;
-        default: return *s == *p ? isMatch(++s, ++p) : false;
-    }
-    
-    return false;
+	return myMatch(s, p) == 1;
 }
